081f6a82c7d942a0e715eed52ca2e2a312adb818
Eliseo10208 <113640708+Eliseo10208@users.noreply.github.com>
2024-01-21 20:32:35 -0600

    primer commit del lado del servidor

---
+++ server.js
@@ -0,0 +1,24 @@
+const { Socket } = require('dgram');
+const http = require('http');
+ 
+const server = http.createServer();
+
+const io = require('socket.io')(server,{
+    cors:{
+        origin:"*"
+    }
+});
+io.on('connection',(socket)=>{
+    console.log("Se ha conectado un cliente")
+    socket.broadcast.emit('chat_message',{
+        usuari: 'INFO',
+        mensaje :'Se ha conectado un nuevo usuario'
+    })
+    socket.on('chat_message',(data)=>{
+        io.emit('chat_message',data);
+    });
+
+});
+
+server.listen(3000);
+console.log("server corriendo");
